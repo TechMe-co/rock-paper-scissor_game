f51bb40e39992dad3d944e2e3e4bae4a107d4ed0
TechMe-co <prettysmiler6@gmail.com>
2026-01-22 07:09:48 -0500

    Create a pseudocode for the program

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/* PSEUDOCODE FOR THE PROGRAM:
+1. Create a new repository for the program (Rock, Paper, Scissor)
+2. Create a function for the computer that randomly return any of the strings: Rock, 
+Paper, Scissor.
+3. Create a function for the user that required an entry from the user among the strings 
+and return the valid option. 
+4. Create two variables that tracks both computer and user score 
+and initialized the variables to zero.
+5. Create a function that takes the record of the highest score between the user and 
+the computer and return it as the winner.
+6. Create a function that uses function 5 as a variable to delcare the final winner 
+after 5 rounds.
+*/
